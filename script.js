dad359f0c41780287cf4936c2a3e23be0112d79e
Hegde-Guru <124256310+Hegde-Guru@users.noreply.github.com>
2023-08-14 23:50:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 function letterCombinations(input_digit) {
-  //Complete the function
+  // Create a mapping of digits to their corresponding letters
+  const digitToLetters = {
+    '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+    '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
+  };
+
+  // Function to generate letter combinations recursively
+  function backtrack(index, currentCombination) {
+    if (index === input_digit.length) {
+      result.push(currentCombination);
+      return;
+    }
+
+    const letters = digitToLetters[input_digit[index]];
+    for (const letter of letters) {
+      backtrack(index + 1, currentCombination + letter);
+    }
+  }
+
+  const result = [];
+  if (input_digit.length > 0) {
+    backtrack(0, '');
+  }
+  
+  return result.sort();
 }
 
 module.exports = letterCombinations;
